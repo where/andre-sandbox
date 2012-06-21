9cdd1c8f03d37ea98dea8f185e199db7144b1712
Andre <andremaserovic@Andres-MacBook-Pro.local>
2012-06-21 18:07:16 -0400

    Error class fix

--- jaxrs-sample/gen-schema.sh
+++ jaxrs-sample/gen-schema.sh
@@ -4,7 +4,7 @@
 
 sdir1=src/main/java/com/andre/rest/data
 sdir2=src/main/java/com/andre/pphere/data
-classes="$sdir2/Location.java $sdir2/Offer.java $sdir2/BaseObject.java $sdir2/OfferList.java $sdir2/Store.java $sdir2/StoreList.java $sdir1/PagedList.java $sdir1/Link.java $sdir2/Error.java"
+classes="$sdir2/Location.java $sdir2/Offer.java $sdir2/BaseObject.java $sdir2/OfferList.java $sdir2/Store.java $sdir2/StoreList.java $sdir1/PagedList.java $sdir1/Link.java $sdir1/Error.java"
 odir=tmp
 
 mkdir $odir
