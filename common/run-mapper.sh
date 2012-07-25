f04eaadc444b88c648c3d16a99b7d732374cf348
Andre <amesar77@hotmail.com>
2012-07-24 22:21:55 -0400

    latest

--- common/run-mapper.sh
+++ common/run-mapper.sh
@@ -3,4 +3,11 @@
 
 PGM=com.andre.mapper.ObjectMapperDriver
 
-java -cp $CPATH $PGM $* | tee oo
+method=list
+method=all
+method=object
+if [ $# -gt 0 ] ; then
+  method=$1
+  fi
+
+java -cp $CPATH $PGM -m $method | tee oo
