8679d506cb2840ab923d72c8b47e6d3f17f4cb72
Andre <amesar77@hotmail.com>
2012-07-31 23:39:37 -0400

    debug

--- json-schema/java/src/test/java/com/andre/jsonschema/BaseTest.java
+++ json-schema/java/src/test/java/com/andre/jsonschema/BaseTest.java
@@ -65,7 +65,7 @@ public class BaseTest {
 	}
 
 	public Object[][] createFiles(File dir) {
-		Assert.assertNotNull(dir,"Directory "+dir+" is null");
+		Assert.assertNotNull(dir,"Directory is null");
 		Assert.assertTrue(dir.exists(),"Directory "+dir.getAbsolutePath()+" does not exist");
 		String [] filenames = dir.list(new MyFilenameFilter());
 		Object[][] objects = new Object[filenames.length][1];
