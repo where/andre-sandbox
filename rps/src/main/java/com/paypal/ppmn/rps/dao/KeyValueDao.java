d3e4ee4faeb1d157796b6e8c290176e59481e338
Andre <amesar77@hotmail.com>
2013-09-15 19:43:43 -0400

    cleanup

--- rps/src/main/java/com/paypal/ppmn/rps/dao/KeyValueDao.java
+++
@@ -1,9 +0,0 @@
-package com.paypal.ppmn.rps.dao;
-
-import java.util.*;
-
-public interface KeyValueDao {
- 	public Object get(String id) throws Exception ;
-	public void put(String id, Object obj) throws Exception ;
-	public void delete(String id) throws Exception;
-}
--- rps/src/main/java/com/paypal/ppmn/rps/data/Field.java
+++
@@ -1,6 +0,0 @@
-package com.paypal.ppmn.rps.data;
-
-public class Field {
-	public String name;
-	public String value;
-}
