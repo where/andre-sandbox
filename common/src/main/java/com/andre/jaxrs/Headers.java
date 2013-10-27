5e11424e700095b05723ccb2dd5909754a639cdd
Andre <amesar77@hotmail.com>
2013-10-27 14:59:23 -0400

    deprecated

--- common/src/main/java/com/andre/jaxrs/Headers.java
+++
@@ -1,6 +0,0 @@
-package com.andre.jaxrs;
-
-public class Headers {
-    public static final String APP_ERROR_CODE = "X-app-error-code" ;
-    public static final String APP_ERROR_MESSAGE = "X-app-error-message" ;
-}
